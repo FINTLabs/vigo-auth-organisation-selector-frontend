085259fa3286c184d7e1d3fac95ba7d85aaf863b
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2021-09-13 10:55:26 +0200

    FIN-48 target (#1)
    * FIN-48 #comment Added deps #in-progress
    
    * FIN-48 #comment Added support for target query parameter #in-progress

---
+++ src/Main.tsx
@@ -0,0 +1,134 @@
+import React, {useEffect, useState} from 'react';
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText
+} from "@material-ui/core";
+import Header from "./components/Header";
+import OrganisationSelector from "./components/OrganisationSelector";
+import {Contract} from "./model/Contract";
+import {useCookies} from "react-cookie";
+import axios from "axios";
+import useQuery from "./hooks/useQuery";
+
+const Main = () => {
+    const [customerContracts, setCustomerContracts] = useState<Contract[]>([]);
+    const [commonContracts, setCommonContracts] = useState<Contract[]>([]);
+    const [cookies, setCookie] = useCookies(['organisation', "rememberMe"]);
+    const [selectedContract, setSelectedContract] = useState<string>(cookies.organisation || "");
+    const [rememberMe, setRememberMe] = useState<boolean>(false);
+    const query = useQuery();
+
+    const getAuthenticationContracts = () => {
+        axios.get<Contract[]>("/api/contract/customer")
+            .then(result => {
+                setCustomerContracts(result.data);
+            })
+        axios.get<Contract[]>("/api/contract/common")
+            .then(result => {
+                setCommonContracts(result.data);
+            })
+    }
+
+    const doRedirect = () => {
+        axios.post("/api/contract/redirect", {
+            "id": selectedContract,
+            "target": query.get("target")
+        },)
+            .then((result) => {
+                window.location = result.data.url;
+            });
+    }
+
+    const handleSelectOrganisation = (organisation: string) => {
+        setSelectedContract(organisation);
+        setCookie("organisation", organisation, {maxAge: 31556926});
+    };
+
+    const handleRememberMe = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRememberMe(event.target.checked);
+        setCookie("rememberMe", event.target.checked, {maxAge: 31556926});
+    };
+
+    useEffect(() => {
+        if (cookies.organisation && cookies.rememberMe) {
+            //window.location = cookies.organisation;
+        }
+        getAuthenticationContracts();
+
+
+    }, [cookies.organisation, cookies.rememberMe]);
+
+    return (
+        <main>
+            <Box display="flex" justifyContent="center" mt={6}>
+                <Box minWidth={320} maxWidth={502} display="flex" justifyContent="center">
+                    <Box
+                        width={1}
+                        padding={4}
+                        border={2}
+                        borderColor="secondary.light"
+                        borderRadius="borderRadius"
+                    >
+                        <Header/>
+
+                        <OrganisationSelector contracts={customerContracts}
+                                              selectedContract={selectedContract}
+                                              setSelectedContract={handleSelectOrganisation}/>
+
+                        <Box display="flex" flexDirection="column"
+                             justifyContent="space-between" mt={2}>
+                            <FormControlLabel
+                                control={<Checkbox checked={rememberMe}
+                                                   onChange={handleRememberMe}
+                                                   name="remeberMe"/>}
+                                label="Husk meg"
+                                disabled={!selectedContract}
+                            />
+                            <Button
+                                size="large"
+                                variant="contained"
+                                color="secondary"
+                                disableElevation
+                                disabled={!selectedContract}
+                                onClick={doRedirect}
+                            >Fortsett</Button>
+                        </Box>
+                        <Box mt={4}>
+                            <Box textAlign="left" fontWeight="fontWeightBold">
+                                Andre påloggingsalternativer
+                            </Box>
+                            <List aria-label="common-providers" disablePadding>
+                                {
+                                    commonContracts.map(contract => (
+                                        <ListItem
+                                            key={contract.cardId}
+                                            button
+                                            disableGutters
+                                            component={Button} href={contract.cardId}
+                                        >
+                                            <ListItemIcon>
+                                                <img
+                                                    src={`data:${contract.image.mimeType};base64, ${contract.image.base64Image}`}
+                                                    alt="Red dot" width={32}/>
+                                            </ListItemIcon>
+                                            <ListItemText primary={contract.displayName}/>
+                                        </ListItem>
+
+                                    ))
+                                }
+                            </List>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </main>
+    );
+};
+
+export default Main;
